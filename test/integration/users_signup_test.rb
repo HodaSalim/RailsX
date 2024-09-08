ca8ee6485f49fe02605fd8c27b10042498e596f4
Hode Salim <hoda.s.salim@gmail.com>
2024-09-08 05:11:16 +0300

    feat: user signup both frontend and backend

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test("Invalid signup form input") do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+    end
+    assert_response :unprocessable_entity
+    assert_template "users/new"
+  end
+
+  test("Valid signup input") do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "test", email: "user@valid.com", password: "foobarr", password_confirmation: "foobarr" } }
+    end
+
+    follow_redirect!
+    assert_template "users/new"
+  end
+end
