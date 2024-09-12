7a8f106374ee145dfa9fc227b68f7ad5df741a92
Hode Salim <hoda.s.salim@gmail.com>
2024-09-13 01:54:24 +0300

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [ :index, :edit, :update, :destroy ]
-  before_action :correct_user, only: [ :edit, :update ]
-  before_action :admin_user, only: [ :destroy ]
+  before_action :logged_in_user, only: [ :index, :edit, :update, :destroy,
+                                        :following, :followers ]
+  before_action :correct_user,   only: [ :edit, :update ]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-  end
-
-  def index
-    @users = User.paginate(page: params[:page])
   end
 
   def new
@@ -17,13 +18,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else  # Handle a successful save.
+      redirect_to root_url
+    else
       render "new", status: :unprocessable_entity
     end
   end
@@ -48,11 +46,28 @@ class UsersController < ApplicationController
     redirect_to users_url, status: :see_other
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow", status: :unprocessable_entity
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow", status: :unprocessable_entity
+  end
+
   private
+
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
+    # Before filters
 
     # Confirms the correct user.
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
 
-  before_save { email.downcase! }
+  before_save :downcase_email
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(
     :email,
@@ -17,6 +28,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
   # Returns the hash digest of the given string.
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -54,6 +66,27 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers).where(part_of_feed, { id: id }).distinct.includes(:user, image_attachment: :blob)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  private
+  def downcase_email
+    self.email = email.downcase
   end
 end
